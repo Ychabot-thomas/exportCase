e0e3c9a82c81d69474b4ebef8c2e00f6c6b6ab9b
Yann Chabot-Thomas <yann.chabothomas@gmail.com>
2021-03-31 14:13:28 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,48 @@
+let recupCase = figma.currentPage.children;
+let afficheCase = recupCase.map((cases, i) => {
+  return {
+    id: cases.id,
+    name: cases.name,
+    x: cases.x,
+    y: cases.y,
+    width: cases.width,
+    height: cases.height,
+    children: cases.children.map((childrenCase, j) => {
+      return {
+        childId: childrenCase.id,
+        childName: childrenCase.name,
+        childX: childrenCase.x,
+        childY: childrenCase.y,
+        childWidth: childrenCase.width,
+        childHeight: childrenCase.height,
+        childOpacity: childrenCase.opacity,
+        childRotation: childrenCase.rotation,
+        childFills: childrenCase.fills,
+        // Text //
+        childFontName: childrenCase.fontName ? childrenCase.fontName : "",
+        childFontSize: childrenCase.fontSize ? childrenCase.fontSize : "",
+        childTextDecoration: childrenCase.textDecoration
+          ? childrenCase.textDecoration
+          : "",
+        childLetterSpacing: childrenCase.letterSpacing
+          ? childrenCase.letterSpacing
+          : "",
+        childLineHeight: childrenCase.lineHeight ? childrenCase.lineHeight : "",
+      };
+    }),
+  };
+});
+
+figma.showUI(__html__);
+
+figma.ui.postMessage(afficheCase);
+
+figma.ui.onmessage = function (msg) {
+  if (msg.type === "export") {
+    console.log("export");
+    figma.closePlugin("Export Finish");
+  } else {
+    console.log("cancel");
+    figma.closePlugin("Cancel");
+  }
+};
